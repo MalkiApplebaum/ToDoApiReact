a8b054436c6bd4c7a5e5caf1676417f989551c19
MalkiApplebaum <m0548548838@gmail.com>
2025-01-19 06:14:42 +0200

    LOGOUT

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link ,useNavigate} from 'react-router-dom';
 import HomePage from './components/HomePage';
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // מחיקת הטוקן מ-sessionStorage
+    sessionStorage.removeItem('token');
+    // הפניה לדף ההתחברות
+    navigate('/');
+  };
+
   return (
     <Router>
       <div>
@@ -12,6 +22,8 @@ function App() {
         <Link to="/homePage" className="nav-btn">TASKS</Link>
         <Link to="/" className="nav-btn">SIGN IN</Link>
         <Link to="/signup" className="nav-btn">SIGN UP</Link>
+        <button onClick={handleLogout} className="nav-btn">LOGOUT</button>
+
         </nav>
 
         <Routes>
@@ -24,4 +36,10 @@ function App() {
   );
 }
 
-export default App;
+const AppWrapper = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+export default AppWrapper;
